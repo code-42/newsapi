7d3ed2407fa18f347b207dca281d0feb913d8439
Edward Dupre <edwd42@gmail.com>
2017-07-26 19:08:02 +0000

    having issues because of move folder

--- resources/script.js
+++ resources/script.js
@@ -13,7 +13,6 @@ $(document).ready(function(){
             var sources = response.sources;
             // console.log(sources[2]);
             var html = "<br><select class='form-control' id='src'>";
-            // html += "<option value='' disabled selected hidden>Please select your news here</option>";
             $.each(sources, function(index, source){
                 // console.log(source);
                 html += "<option value='"+source.id+"'>" + source.name + "</option>";
@@ -42,7 +41,7 @@ $(document).ready(function(){
                 var articles = response.articles;
                 var html = "<ul class='list-group'>"
                 $.each(articles, function(index, article){
-                    html += "<li class='list-group-item'>" + "<a href='"+article.url+"' target='_blank'>"+article.title+"</a>" + "</li>"
+                    html += "<li class='list-item'>" + "<a href='"+article.url+"'>"+article.title+"</a>" + "</li>"
                 })
                 html += "</ul>";
                 $("#articles").html(html);
@@ -50,3 +49,57 @@ $(document).ready(function(){
         })
     })    
 });
+
+
+// $(document).ready(function(){
+//     var apiKey = "2117363bf3ed40fbb54a480ca6852fbc";
+//     var url = "https://newsapi.org/v1/sources";
+//     var data = {language:"en",country:"us"};
+
+//     $.ajax({
+//         url: url,
+//         data: data,
+//         type: "GET",
+//         success: function(response){
+//             // console.log(response);
+//             // console.log(response.sources[2]);
+//             var sources = response.sources;
+//             // console.log(sources[2]);
+//             var html = "<br><select class='form-control' id='src'>";
+//             // html += "<option value='' disabled selected hidden>Please select your news here</option>";
+//             $.each(sources, function(index, source){
+//                 // console.log(source);
+//                 html += "<option value='"+source.id+"'>" + source.name + "</option>";
+//                 // console.log("source.id == " + source.id);
+//                 // srcId = source.id;
+//                 // console.log("21.srcId == " + srcId);
+//             })
+//             html += "</select>";
+//             // console.log(html);
+//             $(".form-group").html(html);
+//         }
+//     })
+
+//     // $("#source").submit(function(event){
+//     $("#source").change(function(event){
+//         event.preventDefault();
+//         var id = $('#src').val();
+//         var url = "https://newsapi.org/v1/articles";
+//         var data = {apiKey: apiKey, source: id};
+//         $.ajax({
+//             url:url,
+//             data: data,
+//             type: "GET",
+//             success: function(response){
+//                 // console.log(response);
+//                 var articles = response.articles;
+//                 var html = "<ul class='list-group'>"
+//                 $.each(articles, function(index, article){
+//                     html += "<li class='list-group-item'>" + "<a href='"+article.url+"' target='_blank'>"+article.title+"</a>" + "</li>"
+//                 })
+//                 html += "</ul>";
+//                 $("#articles").html(html);
+//             }
+//         })
+//     })    
+// });
